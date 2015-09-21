d884d62c1ff64d8429491614ccd0f7c02c9b64f5
Thomas Cantonnet <thomas.cantonnet@swissengine.ch>
2015-09-21 14:23:51 +0200

    Abstract Result

---
+++ src/Places/Result/AbstractResult.php
@@ -0,0 +1,32 @@
+<?php
+namespace GoogleMapsApi\Result;
+
+/**
+ * Class AbstractResult
+ * @package GoogleMapsApi\Result
+ */
+abstract class AbstractResult
+{
+    const REQUEST_ZERO_RESULTS = 'ZERO_RESULTS';
+
+    const REQUEST_OK = 'OK';
+
+    /**
+     * Check Google API response
+     * @param $object
+     * @return bool
+     */
+    public function checkRequestStatus($object)
+    {
+        if (!in_array($object->getStatus(), [REQUEST_ZERO_RESULTS, REQUEST_OK])) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string
+     */
+    abstract public function getStatus();
+}
