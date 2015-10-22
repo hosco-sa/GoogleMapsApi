376e080415397b90ad654ac96c09828d5712477f
Thomas Cantonnet <thomas@hosco.com>
2015-10-22 09:42:20 +0200

    Fixes

--- src/Places/Object/PlaceAddressObject.php
+++ src/Places/Object/PlaceAddressObject.php
@@ -32,35 +32,35 @@ class PlaceAddressObject
     {
         // Set possible options
         $options = new OptionsResolver();
-        $options->setDefined([
-            'locality',
-            'sublocality',
-            'sublocality_level_1',
-            'sublocality_level_2',
-            'sublocality_level_3',
-            'sublocality_level_4',
-            'sublocality_level_5',
-            'postal_code',
-            'postal_town',
-            'neighborhood',
-            'premise',
-            'subpremise',
-            'natural_feature',
-            'airport',
-            'park',
-            'point_of_interest',
-            'ward',
-            'street_address',
-            'route',
-            'intersection',
-            'political',
-            'country',
-            'administrative_area_level_1',
-            'administrative_area_level_2',
-            'administrative_area_level_3',
-            'administrative_area_level_4',
-            'administrative_area_level_5',
-            'colloquial_area',
+        $options->setDefaults([
+            'locality' => '',
+            'sublocality' => '',
+            'sublocality_level_1' => '',
+            'sublocality_level_2' => '',
+            'sublocality_level_3' => '',
+            'sublocality_level_4' => '',
+            'sublocality_level_5' => '',
+            'postal_code' => '',
+            'postal_town' => '',
+            'neighborhood' => '',
+            'premise' => '',
+            'subpremise' => '',
+            'natural_feature' => '',
+            'airport' => '',
+            'park' => '',
+            'point_of_interest' => '',
+            'ward' => '',
+            'street_address' => '',
+            'route' => '',
+            'intersection' => '',
+            'political' => '',
+            'country' => '',
+            'administrative_area_level_1' => '',
+            'administrative_area_level_2' => '',
+            'administrative_area_level_3' => '',
+            'administrative_area_level_4' => '',
+            'administrative_area_level_5' => '',
+            'colloquial_area' => '',
         ]);
 
         // Process Google API Result format
@@ -86,70 +86,70 @@ class PlaceAddressObject
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getLocality($short = true)
     {
-        return $this->data['locality'][$short ? 'short_name' : 'long_name'];
+        return $this->data['locality'] ? $this->data['locality'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getSubLocality($short = true)
     {
-        return $this->data['sublocality'][$short ? 'short_name' : 'long_name'];
+        return $this->data['sublocality'] ? $this->data['sublocality'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getPostalCode($short = true)
     {
-        return $this->data['postal_code'][$short ? 'short_name' : 'long_name'];
+        return $this->data['postal_code'] ? $this->data['postal_code'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getCountry($short = true)
     {
-        return $this->data['country'][$short ? 'short_name' : 'long_name'];
+        return $this->data['country'] ? $this->data['country'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getAdminLevel1($short = true)
     {
-        return $this->data['administrative_area_level_1'][$short ? 'short_name' : 'long_name'];
+        return $this->data['administrative_area_level_1'] ? $this->data['administrative_area_level_1'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getAdminLevel2($short = true)
     {
-        return $this->data['administrative_area_level_2'][$short ? 'short_name' : 'long_name'];
+        return $this->data['administrative_area_level_2'] ? $this->data['administrative_area_level_2'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getAdminLevel3($short = true)
     {
-        return $this->data['administrative_area_level_3'][$short ? 'short_name' : 'long_name'];
+        return $this->data['administrative_area_level_3'] ? $this->data['administrative_area_level_3'][$short ? 'short_name' : 'long_name'] : '';
     }
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getLowestAdminLevel($short = true)
     {
@@ -158,7 +158,7 @@ class PlaceAddressObject
 
     /**
      * @param bool|true $short
-     * return string
+     * @return string
      */
     public function getHighestAdminLevel($short = true)
     {
